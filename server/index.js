77c9a3f73e2dae019b55bbdeb275d74403b76adb
Sam Zhao <zhao.shengjun@gmail.com>
2018-05-31 22:35:08 +1000

    basic server setup

---
+++ server/index.js
@@ -0,0 +1,16 @@
+import express from "express";
+import jwt from "express-jwt";
+import cors from "cors";
+import jwks from "jwks-rsa";
+import bodyParser from "body-parser";
+
+const PORT = 4000;
+
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
