369c9b16cae795d64d0b2edc91b6815a4e7a9a38
Sam Zhao <zhao.shengjun@gmail.com>
2018-06-02 22:12:15 +1000

    product list

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -5,9 +5,10 @@ import { NgModule } from "@angular/core";
 import { AppComponent } from "./app.component";
 import { CategoryListComponent } from './category-list/category-list.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { ProductListComponent } from './product-list/product-list.component';
 
 @NgModule({
-  declarations: [AppComponent, CategoryListComponent, DashboardComponent],
+  declarations: [AppComponent, CategoryListComponent, DashboardComponent, ProductListComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
