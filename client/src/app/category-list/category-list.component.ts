3dea0af5ed04a149d6e870105159a5ef55b43156
Sam Zhao <zhao.shengjun@gmail.com>
2018-06-02 21:58:21 +1000

    add category

--- client/src/app/category-list/category-list.component.ts
+++ client/src/app/category-list/category-list.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-category-list',
-  templateUrl: './category-list.component.html',
-  styleUrls: ['./category-list.component.css']
+  selector: "app-category-list",
+  templateUrl: "./category-list.component.html",
+  styleUrls: ["./category-list.component.css"]
 })
 export class CategoryListComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
