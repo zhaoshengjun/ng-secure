9c815f11d81cd3c0d4b9427c36171d9ddfde6ff4
Sam Zhao <zhao.shengjun@gmail.com>
2018-06-02 21:56:35 +1000

    add category list

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,9 +3,10 @@ import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 
 import { AppComponent } from "./app.component";
+import { CategoryListComponent } from './category-list/category-list.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, CategoryListComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
