9f1e95d8e4b0536809cfcc129feb2753d6147248
Sam Zhao <zhao.shengjun@gmail.com>
2018-06-02 22:07:47 +1000

    dashboard component

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,9 +4,10 @@ import { NgModule } from "@angular/core";
 
 import { AppComponent } from "./app.component";
 import { CategoryListComponent } from './category-list/category-list.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
-  declarations: [AppComponent, CategoryListComponent],
+  declarations: [AppComponent, CategoryListComponent, DashboardComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
