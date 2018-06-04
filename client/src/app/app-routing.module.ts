66359fe7b04f1fa2abdab87afa46a90e5f303cec
Sam Zhao <zhao.shengjun@gmail.com>
2018-06-04 22:25:43 +1000

    update routing

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
+import { CategoryListComponent } from "./category-list/category-list.component";
+import { ProductDetailComponent } from "./product/product-detail.component";
+import { ProductListComponent } from "./product/product-list.component";
+import { DashboardComponent } from "./dashboard/dashboard.component";
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "dashboard", component: DashboardComponent },
+  { path: "products", component: ProductListComponent },
+  { path: "productDetail/:id", component: ProductDetailComponent },
+  { path: "categories", component: CategoryListComponent },
+  { path: "", redirectTo: "dashboard", pathMatch: "full" },
+  { path: "**", component: DashboardComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
