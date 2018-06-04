fe6e41dc65c50e2c950eecd32787456ae2ca39cd
Sam Zhao <zhao.shengjun@gmail.com>
2018-06-04 22:22:43 +1000

    update product list

--- client/src/app/product/product-list.component.ts
+++ client/src/app/product/product-list.component.ts
@@ -1,15 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { ProductService } from "./product.service";
+import { Component, OnInit } from "@angular/core";
+import { Product } from "./product";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-product-list',
-  templateUrl: './product-list.component.html',
-  styleUrls: ['./product-list.component.css']
+  selector: "app-product-list",
+  templateUrl: "./product-list.component.html",
+  styleUrls: ["./product-list.component.css"]
 })
 export class ProductListComponent implements OnInit {
+  products: Product[];
 
-  constructor() { }
+  constructor(private productService: ProductService, private router: Router) {}
 
   ngOnInit() {
+    this.getProducts();
   }
 
+  private getProducts() {
+    this.productService
+      .getProducts()
+      .subscribe(products => (this.products = products));
+  }
+
+  addProduct() {
+    this.router.navigate(["/productDetail", -1]);
+  }
+  deleteProduct(id: number) {
+    if (confirm("Delete this product?")) {
+      this.productService
+        .deleteProduct(id)
+        .subscribe(
+          () => (this.products = this.products.filter(p => p.productId != id))
+        );
+    }
+  }
 }
