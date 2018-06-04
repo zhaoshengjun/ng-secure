72b889409891318aa7e90ab51207633b819fadd9
Sam Zhao <zhao.shengjun@gmail.com>
2018-06-04 22:31:48 +1000

    fix import issue

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { AppRoutingModule } from "./app-routing.module";
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
 
 import { AppComponent } from "./app.component";
-import { CategoryListComponent } from './category-list/category-list.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { ProductListComponent } from './product-list/product-list.component';
-import { ProductDetailComponent } from './product/product-detail.component';
+import { CategoryListComponent } from "./category-list/category-list.component";
+import { DashboardComponent } from "./dashboard/dashboard.component";
+import { ProductListComponent } from "./product/product-list.component";
+import { ProductDetailComponent } from "./product/product-detail.component";
 
 @NgModule({
-  declarations: [AppComponent, CategoryListComponent, DashboardComponent, ProductListComponent, ProductDetailComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  declarations: [
+    AppComponent,
+    CategoryListComponent,
+    DashboardComponent,
+    ProductListComponent,
+    ProductDetailComponent
+  ],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
