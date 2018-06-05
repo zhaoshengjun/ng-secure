42a1f7d0bcb04898d2ed6594be05d77f0ae68295
Sam Zhao <zhao.shengjun@gmail.com>
2018-06-05 23:23:27 +1000

    add login page

--- client/src/app/security/login.component.ts
+++ client/src/app/security/login.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { SecurityService } from "./security.service";
+import { AppUserAuth } from "./app-user-auth";
+import { Component, OnInit } from "@angular/core";
+import { AppUser } from "./app-user";
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  selector: "app-login",
+  templateUrl: "./login.component.html",
+  styleUrls: ["./login.component.css"]
 })
 export class LoginComponent implements OnInit {
+  securityObject: AppUserAuth = null;
+  user: AppUser = new AppUser();
+  constructor(private securityService: SecurityService) {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  login() {
+    this.securityService
+      .login(this.user)
+      .subscribe(resp => (this.securityObject = resp));
   }
-
 }
