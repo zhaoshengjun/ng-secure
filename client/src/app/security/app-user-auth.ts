077c42a34e89eed6a7bb5bf15d13204ea8b18c47
Sam Zhao <zhao.shengjun@gmail.com>
2018-06-05 22:50:50 +1000

    add app user auth class

---
+++ client/src/app/security/app-user-auth.ts
@@ -0,0 +1,10 @@
+export class AppUserAuth {
+  userName: string = "";
+  bearerToken: string = "";
+  isAuthenticated: boolean = false;
+  canAccessProduct: boolean = false;
+  canAddProduct: boolean = false;
+  canSaveProduct: boolean = false;
+  canAccessCategories: boolean = false;
+  canAddCategory: boolean = false;
+}
