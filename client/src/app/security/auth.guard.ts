13419f4454a4c42ced591ec56f9956fc2a67163c
Sam Zhao <zhao.shengjun@gmail.com>
2018-06-06 23:09:23 +1000

    update auth guard

--- client/src/app/security/auth.guard.ts
+++ client/src/app/security/auth.guard.ts
@@ -1,14 +1,25 @@
-import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
-import { Observable } from 'rxjs';
+import { SecurityService } from "./security.service";
+import { Injectable } from "@angular/core";
+import {
+  CanActivate,
+  ActivatedRouteSnapshot,
+  RouterStateSnapshot
+} from "@angular/router";
+import { Observable } from "rxjs";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class AuthGuard implements CanActivate {
+  constructor(private securityService: SecurityService) {}
   canActivate(
     next: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
-    return true;
+    state: RouterStateSnapshot
+  ): Observable<boolean> | Promise<boolean> | boolean {
+    let claimType: string = next.data["claimType"];
+    return (
+      this.securityService.securityObject.isAuthenticated &&
+      this.securityService.securityObject[claimType]
+    );
   }
 }
