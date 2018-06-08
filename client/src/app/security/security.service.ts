25f111410259fac3f775ee799e590b89ea045ff5
Sam Zhao <zhao.shengjun@gmail.com>
2018-06-08 22:51:37 +1000

    refactor SecurityService

--- client/src/app/security/security.service.ts
+++ client/src/app/security/security.service.ts
@@ -24,11 +24,7 @@ export class SecurityService {
       userName: "",
       bearerToken: "",
       isAuthenticated: false,
-      canAccessProduct: false,
-      canAddProduct: false,
-      canSaveProduct: false,
-      canAccessCategories: false,
-      canAddCategory: false
+      claims: []
     };
     this.securityObject = emptyObject;
     localStorage.removeItem("bearerToken");
