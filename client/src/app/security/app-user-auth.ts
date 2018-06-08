7bb03999b992efef238471b243f6403ce54e6130
Sam Zhao <zhao.shengjun@gmail.com>
2018-06-08 22:50:40 +1000

    refactor AppUserAuth

--- client/src/app/security/app-user-auth.ts
+++ client/src/app/security/app-user-auth.ts
@@ -1,10 +1,7 @@
+import { AppUserClaim } from "./app-user-claim";
 export class AppUserAuth {
   userName: string = "";
   bearerToken: string = "";
   isAuthenticated: boolean = false;
-  canAccessProduct: boolean = false;
-  canAddProduct: boolean = false;
-  canSaveProduct: boolean = false;
-  canAccessCategories: boolean = false;
-  canAddCategory: boolean = false;
+  claims: AppUserClaim[] = [];
 }
