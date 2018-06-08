a48150ee6d511d4fdd590a8b36e7dbce8b788ed5
Sam Zhao <zhao.shengjun@gmail.com>
2018-06-08 23:17:24 +1000

    update app component to use multiple structural directives via ngContainer

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,3 +1,5 @@
+import { SecurityService } from "./security/security.service";
+import { AppUserAuth } from "./security/app-user-auth";
 import { Component } from "@angular/core";
 
 @Component({
@@ -6,5 +8,13 @@ import { Component } from "@angular/core";
   styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title: string = "Training Company"
+  title: string = "Training Company";
+  securityObject: AppUserAuth = null;
+
+  constructor(private securityService: SecurityService) {
+    this.securityObject = securityService.securityObject;
+  }
+  logtout() {
+    this.securityService.logout();
+  }
 }
